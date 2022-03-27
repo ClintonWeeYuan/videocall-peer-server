1ea17e97fc302e2e516c3faf15e91f6fc27dc6ea
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-03-27 12:22:02 +0800

    Used simple node package

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
-
+const PORT = process.env.PORT || 9000
 app.get('/', (req, res, next) => res.send('Hello world!'));
 
 // =======
 
-const server = app.listen(9000);
+const server = app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
 
 const peerServer = ExpressPeerServer(server, {
     path: '/myapp'
